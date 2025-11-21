1627e2af4bdbc187224ef4138aa12fc4d8cd86e6
Jovana Hernandez <jhernandez@Jovanas-MBP.lan>
2025-11-21 15:39:43 -0500

    Change Direction Enum values to align with Maze grid

--- Position.py
+++ Position.py
@@ -1,9 +1,9 @@
 from enum import Enum
 class Direction(Enum):
-    UP = (0, 1)
-    DOWN = (0, -1)
-    LEFT = (-1, 0)
-    RIGHT = (1, 0)
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
 
 class Position:
     def __init__(self, x, y):
